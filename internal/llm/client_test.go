493aa8abcd8d0ff309c27976df156fd7322db65a
pexlor <1018734423@qq.com>
2026-07-05 14:51:43 +0800

    add openai-compat

--- internal/llm/client_test.go
+++ internal/llm/client_test.go
@@ -7,8 +7,6 @@ import (
 func main() {
 	client, _ := NewClient(&ModelParm{
 		Protocol:  "openai-compat",
-		BaseURL:   "https://llm-lgsv9uhdbprfr0vv.cn-beijing.maas.aliyuncs.com/compatible-mode/v1",
-		APIKey:    "sk-72683ab6f2174c81bc7d05d13b4c7296",
 		ModelName: "qwen-plus",
 	})
 	eventChan, errChan := client.Stream(&StreamRequest{})
